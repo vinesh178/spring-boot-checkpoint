3651f0b84fa6b132942880439dd327ee4317c0bd
kumarv14 <vineshk83@gmail.com>
2021-03-29 15:33:51 +1100

    Code cleanup

--- src/main/java/com/example/checkpoint/model/CheckpointRepository.java
+++ src/main/java/com/example/checkpoint/model/CheckpointRepository.java
@@ -2,9 +2,6 @@ package com.example.checkpoint.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 
 public interface CheckpointRepository extends JpaRepository<Checkpoint, Long> {
